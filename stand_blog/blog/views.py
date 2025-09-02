bb2f388509561ac634506be9933b3f2d2d42d7c6
Hossien7 <139698226+Hossien7@users.noreply.github.com>
2025-09-02 17:12:47 +0330

    UPDATE: Display recent posts on home and article detail pages
    Updated views and templates to show the three most recent posts on both the home page and article detail page. Replaced static blog post content in the home page template with dynamic data from the Article model.

--- stand_blog/blog/views.py
+++ stand_blog/blog/views.py
@@ -4,4 +4,7 @@ from blog.models import Article
 
 def post_detail(request, slug):
     article = Article.objects.get(slug=slug)
-    return render(request, 'blog/article-details.html', {'article': article})
+    recent_post = Article.objects.all().order_by('-created')[:3]
+    return render(request, 'blog/article-details.html', {'article': article, 'recent_posts': recent_post})
+
+
--- stand_blog/home/views.py
+++ stand_blog/home/views.py
@@ -6,9 +6,10 @@ def home(request):
     # obj = Article.objects.all().filter(status=True)
     # obj = Article.objects.published()
     obj = Article.custom_object.all()
+    recent_post = Article.objects.all().order_by('-created')[:3]
     # obj1 = New.objects.get(id=1)
     # obj1.title = 'Farid rezaii'
     # obj1.save()
-    return render(request, 'home/index.html', {'articles': obj})
+    return render(request, 'home/index.html', {'articles': obj, 'recent_posts': recent_post})
 
 
