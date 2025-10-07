4a61dcf92d1530865d928eb7223df6f6aaeb1add
Hossien7 <139698226+Hossien7@users.noreply.github.com>
2025-10-07 21:39:43 +0330

    Add widget to text field in ContactUsForm
    Updated the 'text' field in ContactUsForm to use a Textarea widget with form-control class and placeholder, improving the form's UI consistency.

--- stand_blog/blog/form.py
+++ stand_blog/blog/form.py
@@ -3,7 +3,7 @@ from django.core.validators import ValidationError
 
 class ContactUsForm(forms.Form):    # Define a form class
     name = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
-    text = forms.CharField(max_length=10)
+    text = forms.CharField(max_length=10, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Text'}))
     
 
     def clean(self):    # Custom validation logic
